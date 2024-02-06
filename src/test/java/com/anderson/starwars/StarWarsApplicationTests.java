080610d5feb65dbeece4e084fc12865900128afe
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-02-06 19:29:25 -0300

    feat: Project created

---
+++ src/test/java/com/anderson/starwars/StarWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderson.starwars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
