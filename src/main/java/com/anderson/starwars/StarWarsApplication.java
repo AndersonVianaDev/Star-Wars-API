9f3a9862b30170e3ee27d243b8a7ba2eb1ece0fb
Anderson Viana <andersonp.viana.contato@gmail.com>
2024-02-07 16:10:50 -0300

    feat: adding swagger

--- src/main/java/com/anderson/starwars/StarWarsApplication.java
+++ src/main/java/com/anderson/starwars/StarWarsApplication.java
@@ -1,11 +1,14 @@
 package com.anderson.starwars;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@OpenAPIDefinition(info = @Info(title = "star-wars-api", version = "1", description = "API designed to manage tasks"))
 public class StarWarsApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/anderson/starwars/external/controller/PlanetController.java
+++ src/main/java/com/anderson/starwars/external/controller/PlanetController.java
@@ -4,6 +4,8 @@ import com.anderson.starwars.core.world.dtos.PlanetDTO;
 import com.anderson.starwars.core.world.dtos.PlanetResponseDTO;
 import com.anderson.starwars.core.world.model.Planet;
 import com.anderson.starwars.core.world.service.PlanetService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -14,6 +16,7 @@ import java.util.UUID;
 
 @Controller
 @RequestMapping("api/v1/world")
+@Tag(name = "star-wars-api")
 public class PlanetController {
 
     private final PlanetService service;
@@ -23,6 +26,7 @@ public class PlanetController {
     }
 
     @PostMapping("/create")
+    @Operation(summary = "create planet", method = "POST")
     public ResponseEntity<Void> create(@RequestBody PlanetDTO dto) {
         this.service.create(dto);
 
@@ -30,6 +34,7 @@ public class PlanetController {
     }
 
     @GetMapping("/find/id/{id}")
+    @Operation(summary = "find planet by id", method = "GET")
     public ResponseEntity<PlanetResponseDTO> findById(@PathVariable UUID id) {
         Planet planet = this.service.findById(id);
 
@@ -39,6 +44,7 @@ public class PlanetController {
     }
 
     @GetMapping("find/name/")
+    @Operation(summary = "find planet by name", method = "GET")
     public ResponseEntity<PlanetResponseDTO> findByName(@RequestParam(name = "name") String name) {
         Planet planet = this.service.findByName(name);
 
@@ -48,6 +54,7 @@ public class PlanetController {
     }
 
     @DeleteMapping("delete/id/{id}")
+    @Operation(summary = "delete planet", method = "DELETE")
     public ResponseEntity<Void> delete(@PathVariable UUID id) {
         this.service.delete(id);
 
@@ -55,6 +62,7 @@ public class PlanetController {
     }
 
     @GetMapping("findAll")
+    @Operation(summary = "find all planet", method = "GET")
     public ResponseEntity<List<PlanetResponseDTO>> findAll() {
         List<Planet> list = this.service.findAll();
 
